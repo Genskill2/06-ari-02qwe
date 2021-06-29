0e12d36f82af7bc7248f4c07a339cd8b7cd48c70
02qwe <84759939+02qwe@users.noreply.github.com>
2021-06-29 17:49:10 +0530

    Update ari.c

--- ari.c
+++ ari.c
@@ -2,12 +2,12 @@ string ari(string st)
 {
      char *s=st;int wrd_ct=0;int snt_count=0;int ch=0;
      for(int i=0;*(s+i)!='\0';++i)
-     {   if(*(st+i)=='')
+     {   if(*(st+i)==' ')
               wrd_ct++;
-           if (*(st+i)=='')
+           if (*(st+i)=='.')
               snt_count++;
      }
- 
+                 ch=i;
                 int ari=(4.71*ch/wrd_ct)+(.5*wrd_ct/snt_count)-21.43; 
         
                 string class[]={"Kindergarten","First/Second Grade","Third Grade","Fourth Grade","Fifth Grade","Sixth Grade","Seventh Grade","Eighth Grade","Ninth Grade","Tenth Grade","Eleventh Grade","Twelfth grade","College student","Professor"};
